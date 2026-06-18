6d746891ef302bcdf05766c1aed8c794b57388fa
dmsovenko-ship-it <dmsovenko@gmail.com>
2026-06-18 13:11:30 +0300

    fix: recalc warp distance in catch-all alignment path

--- src/eve-server/system/DestinyManager.cpp
+++ src/eve-server/system/DestinyManager.cpp
@@ -241,6 +241,9 @@ void DestinyManager::ProcessState() {
                             mySE->GetName(), mySE->GetID());
                 }
                 m_shipHeading = toVec;
+                // Recalculate distance: ship may have drifted 10+ seconds at
+                // full speed while trying to align.
+                m_targetDistance = GPoint(m_position).distance(m_targetPoint);
                 InitWarp();
                 return;
             }
