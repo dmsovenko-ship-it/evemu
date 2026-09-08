67d03e7b0617b079305cbcd7a2fa382a2451b6f7
dmsovenko-ship-it <dmsovenko@gmail.com>
2026-09-08 16:13:58 +0300

    fix(migrations): account_ban_reason idempotent (ADD COLUMN IF NOT EXISTS)

--- sql/migrations/20260908000006-account_ban_reason.sql
+++ sql/migrations/20260908000006-account_ban_reason.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
 -- Store the admin's ban reason so the client's login error can show it.
-ALTER TABLE account ADD COLUMN banReason varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE account ADD COLUMN IF NOT EXISTS banReason varchar(255) NOT NULL DEFAULT '';
 -- +migrate Down
 ALTER TABLE account DROP COLUMN banReason;
